4c612b220b0315ae46ee81d4961bdb605ff473a8
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-06-24 09:37:19 +0700

    ajax call api

---
+++ src/main/java/com/codegym/service/IGeneralService.java
@@ -0,0 +1,20 @@
+package com.codegym.service;
+
+import com.codegym.exception.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IGeneralService<T> {
+    List<T> findALl();
+
+    Page<T> findALl(Pageable pageable);
+
+    T findById(Long id) throws NotFoundException;
+
+    T save(T t);
+
+    void deleteById(Long id);
+
+}
